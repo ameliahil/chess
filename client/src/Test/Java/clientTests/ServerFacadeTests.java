2cad68d05c049216d44332c82ee223786066e424
ameliahil <ah683@byu.edu>
2024-03-22 17:29:57 -0600

    more tests passed

--- client/src/Test/Java/clientTests/ServerFacadeTests.java
+++ client/src/Test/Java/clientTests/ServerFacadeTests.java
@@ -1,16 +1,23 @@
 package clientTests;
 
+import Requests.LoginRequest;
+import Requests.LoginResponse;
 import UI.ServerFacade;
-import dataAccess.DataAccessException;
+import dataAccess.*;
+import model.UserData;
 import org.junit.jupiter.api.*;
 import server.Server;
 import static org.junit.jupiter.api.Assertions.*;
+
 
 
 public class ServerFacadeTests {
 
     private static Server server;
     private static ServerFacade serverFacade;
+    SQLUserDAO userDAO = new SQLUserDAO();
+    SQLGameDAO gameDAO = new SQLGameDAO();
+    SQLAuthDAO authDAO = new SQLAuthDAO();
 
     @BeforeAll
     public static void init() {
@@ -31,39 +38,58 @@ public class ServerFacadeTests {
     public void clearTest() {
         assertDoesNotThrow(() -> {serverFacade.clear();});
     }
-    public void registerPosTest() {
-
+    @Test
+    public void registerPosTest() throws DataAccessException {
+        UserData user = new UserData("user","pass","email");
+        LoginResponse loginResponse = serverFacade.registration(user);
+        assertEquals(authDAO.getUser(loginResponse.authToken()),"user");
     }
-    public void registerNegTest() {
-
+    @Test
+    public void registerNegTest() throws DataAccessException {
+        UserData user = new UserData("newUser","pass","email");
+        LoginResponse loginResponse = serverFacade.registration(user);
+        assertNotEquals(authDAO.getUser(loginResponse.authToken()),"user");
     }
-    public void loginPosTest() {
-
+    @Test
+    public void loginPosTest() throws DataAccessException {
+        LoginRequest loginRequest = new LoginRequest("user", "pass");
+        LoginResponse loginResponse = serverFacade.login(loginRequest);
+        assertNotNull(loginResponse.authToken());
     }
+    @Test
     public void loginNegTest() {
-
+        LoginRequest loginRequest = new LoginRequest("user", "past");
+        assertThrows(DataAccessException.class,()->{serverFacade.login(loginRequest);});
     }
+    @Test
     public void logoutPosTest() {
 
     }
+    @Test
     public void logoutNegTest() {
 
     }
+    @Test
     public void listGamesPosTest() {
 
     }
+    @Test
     public void listGamesNegTest() {
 
     }
+    @Test
     public void createGamePosTest() {
 
     }
+    @Test
     public void createGameNegTest() {
 
     }
+    @Test
     public void joinGamePosTest() {
 
     }
+    @Test
     public void joinGameNegTest() {
 
     }
